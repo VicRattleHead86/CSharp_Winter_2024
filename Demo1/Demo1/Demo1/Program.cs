0b4a57b3938aecee9e97f147ff8d4d58ab52ae4a
VicRattleHead86 <145300746+VicRattleHead86@users.noreply.github.com>
2024-11-20 00:19:50 -0700

    Add files via upload

---
+++ Demo1/Demo1/Demo1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Program Demo1
+ * Function Demonstration of basic setup
+ * 
+ * Reg Westly
+ * CMPE1300 A03
+ * Jan 8, 2024
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare Variables
+            int iCounterOfTheNumberOfBalls = 0;             // Counter for number of times
+            string sBallName;                               // name of the ball
+            bool bWork = false;                             // result of tryparse
+
+            // Get the data
+            Console.Write("Please enter the name of ball: ");
+            sBallName = Console.ReadLine();
+            Console.Write("Please enter the number of balls: ");
+            bWork = int.TryParse(Console.ReadLine(), out iCounterOfTheNumberOfBalls);   // try to change input to integer result in variable
+            
+            // write out results
+            Console.WriteLine("\n\n\n\t\t\t" + sBallName + " " + iCounterOfTheNumberOfBalls + " " + bWork);
+
+            //Wait
+            Console.ReadKey();
+            
+        }
+    }
+}
