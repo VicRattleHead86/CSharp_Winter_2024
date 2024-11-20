9953a49c699b54d11e12a910db76244781828cc0
VicRattleHead86 <145300746+VicRattleHead86@users.noreply.github.com>
2024-11-20 00:19:24 -0700

    Add files via upload

---
+++ CharTypes/CharTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iDigit = 0;
+            string sString = "This 12  #^*^:>'  sds";
+
+            foreach (char ch in sString) 
+            {
+                if (char.IsLetter(ch))          // change to see types
+                {
+                    iDigit++;
+                }
+            }
+            Console.WriteLine(iDigit);
+            Console.ReadKey();
+        }
+    }
+}
